c58de05204c76b168e0d3249466c4c3129b6f4e3
Dmitry <manxxeta@gmail.com>
2018-07-23 17:32:03 -0700

    step into the logic

--- app/app.js
+++ app/app.js
@@ -1,46 +1,56 @@
 'use strict';
 
-
-
-
+// array of images to fill an object
 var imgsArray = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.jpg', 'tauntaun.jpg', 'unicorn.jpg', 'usb.jpg', 'water-can.jpg', 'wine-glass.jpg'];
-var imgsObj = {};
+var imgsObj = [];
 
 
 
 function ImageTracker(img)
 {
-  // imagePath -> ./img/breakfast.jpg
-  this.name = img.split('.'[0]);
-  this.path = img;
-  this.totalClicks = 0;
+    // imagePath -> ./img/breakfast.jpg
+    this.name = img.split('.'[0]);
+    this.path = img;
+    this.totalClicks = 0;
 }
 
 for( var i=0; i < imgsArray.length; i++) 
 {
-  imgsObj.push(new ImageTracker(imgsArray[i]));
+    imgsObj[i] = new ImageTracker(imgsArray[i]);
 }
 
 
 
 // connect all pictures from a page and radio buttons
+// var radioOne = document.getElementById('radioOne');
 
-
-function displayRandomPictures(imgsArray)
+function displayRandomPicture(imgsObj)
 {
+    // --------------randomize first picture-----------------
+    // find out random picture from array
+    var pictureIndexOne = Math.floor(Math.random() * Math.floor(imgsObj.length));
+    // place it to a frame
+    document.getElementById('picOne').src = './img/' + imgsArray[pictureIndexOne];
+    //pop it from array, so it doesn't repeat
+    // imgsArray.splice(pictureIndexOne, 1);
+    // ----------------------------------------------------
 
-  // find out random picture
-  var pictureOneIndex = Math.floor(Math.random() * Math.floor(imgsArray.length));
-  // place it to a page
+    // --------------randomize second picture-----------------
+    // find out random picture from array
+    var pictureIndexTwo = Math.floor(Math.random() * Math.floor(imgsObj.length));
+    // place it to a frame
+    document.getElementById('picTwo').src = './img/' + imgsArray[pictureIndexTwo];
+    //pop it from array, so it doesn't repeat
+    // imgsArray.splice(pictureIndexTwo, 1);
+    // ----------------------------------------------------
 
-  //pop it from array
-  var firstPicture = imgsArray.splice(pictureOneIndex, 1);
-
-
-
-
-
-
-  
+    // --------------randomize third picture-----------------
+    // find out random picture from array
+    var pictureIndexThree = Math.floor(Math.random() * Math.floor(imgsObj.length));
+    // place it to a frame
+    document.getElementById('picThree').src = './img/' + imgsArray[pictureIndexThree];
+    //pop it from array, so it doesn't repeat
+    // imgsArray.splice(pictureIndexThree, 1);
+    // ----------------------------------------------------
 
 }
