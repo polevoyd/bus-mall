35aaae16dde35896bd4214827ef917be3b46b108
Dmitry <manxxeta@gmail.com>
2018-07-23 19:04:59 -0700

    step into the logic

--- app/app.js
+++ app/app.js
@@ -5,52 +5,105 @@ var imgsArray = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfas
 var imgsObj = [];
 
 
+//////////////////////////////////////////////////////////////////////////////////
+/////////////////////CONSTRUCTOR FOR AN IMAGE OBJECT//////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
 
-function ImageTracker(img)
+function ImageTracker(imgName)
 {
     // imagePath -> ./img/breakfast.jpg
-    this.name = img.split('.'[0]);
-    this.path = img;
+    this.name = imgName.split('.'[0]);
+    this.path = imgName;
     this.totalClicks = 0;
 }
 
-for( var i=0; i < imgsArray.length; i++) 
-{
-    imgsObj[i] = new ImageTracker(imgsArray[i]);
-}
+//////////////////////////////////////////////////////////////////////////////////
+/////////////////////DEFINING INTERACTIVE ELEMENTS ON A PAGE//////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+// picture frames
+
+var frameOne = document.getElementById('picOne');
+var frameTwo = document.getElementById('picTwo');
+var frameThree = document.getElementById('picThree');
+
+// radio buttons
+
+var radioOne = document.getElementById('radioOne');
+var radioTwo = document.getElementById('radioTwo');
+var radioThree = document.getElementById('radioThree');
+
+var radioArr = [radioOne, radioTwo, radioThree];
+
+// vote button
+
+var vote = document.getElementById('voteButton');
 
 
 
-// connect all pictures from a page and radio buttons
-// var radioOne = document.getElementById('radioOne');
 
-function displayRandomPicture(imgsObj)
+//////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////FUNCTIONS DEFINITIONS/////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+// display random pictures to a page
+function displayRandomPicture()
 {
     // --------------randomize first picture-----------------
     // find out random picture from array
-    var pictureIndexOne = Math.floor(Math.random() * Math.floor(imgsObj.length));
+    var pictureIndexOne = Math.floor(Math.random() * Math.floor(imgsArray.length));
     // place it to a frame
-    document.getElementById('picOne').src = './img/' + imgsArray[pictureIndexOne];
-    //pop it from array, so it doesn't repeat
-    // imgsArray.splice(pictureIndexOne, 1);
-    // ----------------------------------------------------
-
+    frameOne.src = './img/' + imgsArray[pictureIndexOne];
+    // set a value of radio button as index of that image in our initial array
+    radioOne.setAttribute('value', pictureIndexOne);
+    
     // --------------randomize second picture-----------------
     // find out random picture from array
-    var pictureIndexTwo = Math.floor(Math.random() * Math.floor(imgsObj.length));
+    var pictureIndexTwo = Math.floor(Math.random() * Math.floor(imgsArray.length));
     // place it to a frame
-    document.getElementById('picTwo').src = './img/' + imgsArray[pictureIndexTwo];
-    //pop it from array, so it doesn't repeat
-    // imgsArray.splice(pictureIndexTwo, 1);
-    // ----------------------------------------------------
+    frameTwo.src = './img/' + imgsArray[pictureIndexTwo];
+    radioTwo.setAttribute('value', pictureIndexOne);
 
     // --------------randomize third picture-----------------
     // find out random picture from array
-    var pictureIndexThree = Math.floor(Math.random() * Math.floor(imgsObj.length));
+    var pictureIndexThree = Math.floor(Math.random() * Math.floor(imgsArray.length));
     // place it to a frame
-    document.getElementById('picThree').src = './img/' + imgsArray[pictureIndexThree];
-    //pop it from array, so it doesn't repeat
-    // imgsArray.splice(pictureIndexThree, 1);
-    // ----------------------------------------------------
-
+    frameThree.src = './img/' + imgsArray[pictureIndexThree];
+    radioThree.setAttribute('value', pictureIndexOne);
 }
+
+// what happens after 'vote' clicked
+var clicked = function()
+{
+    // add a total click count to a image
+    for (var radioBtn of radioArr)
+    {
+        if (radioBtn.checked)
+        {
+            // in image object with same index we increment a total click
+            imgsObj[radioBtn.value].totalClicks += 1;
+            // and jump out of loop
+            break;
+        }
+    }
+
+    displayRandomPicture(imgsObj);
+};
+
+// attaching event listener to a 'vote' button
+vote.addEventListener('click', clicked);
+
+
+
+//////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////ACTION/////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+// filling an objects array
+for( var i=0; i < imgsArray.length; i++) 
+{
+    imgsObj.push(new ImageTracker(imgsArray[i]));
+}
+
+// putting random pictures on a screen
+displayRandomPicture();
